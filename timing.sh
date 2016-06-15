33700f11c3994400b0f8e9ea4b8a0bcf8cfb907d
Omar Amer <omar.amer@uconn.edu>
2016-06-15 09:47:18 -0400

    got rid of time stamp

--- timing.sh
+++ timing.sh
@@ -19,7 +19,7 @@ CALL="reaver -i wlan0mon -b $MAC -vv -l 300 -g $ATTEMPTS -d $D -N -S -r $X:$Y"
 filename="$X;$Y-$D.revtest"
 echo "$CALL" >> "./xytime/$filename"
 
-$CALL | ts '[%Y-%m-%d %H:%M:%S]' | tee "./xytime/$filename"
+$CALL | tee "./xytime/$filename"
 
 airmon-ng stop wlan0mon
 service network-manager restart
